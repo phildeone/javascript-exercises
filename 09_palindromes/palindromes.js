55a16f8bc7b579979a624d1f7ad33f75921f1703
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-12-30 18:36:44 +0100

    Update palindromes function in palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+// const palindromes = function (string) {
+//     let re = /[\w_]/g;
+//     string = string.toLowerCase().replace(re, '');
+//     let len = string.length;
+//     for(let i = 0; i < len / 2; i++){
+//         if (string[i] !== string[len - 1 - i]){
+//             return false;
+//         }
+//     }
+//     return true;
+// };
 
-};
+const palindromes = function(string) {
+    const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (
+      processedString
+        .split("")
+        .reverse()
+        .join("") == processedString
+    );
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
