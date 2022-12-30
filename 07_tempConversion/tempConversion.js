401bb1dc786d9167bd47e5fb502b6c2285199e6f
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-12-30 09:23:28 +0100

    Update tempCoversion,js to pass TDD

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFarenheit) {
+  const celsius = ((tempInFarenheit - 32) / 1.8);
+  const cels = parseFloat(celsius.toFixed(1));
+  return cels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  const fahrenheit = ((tempInCelsius * 1.8) + 32);
+  const fahr = parseFloat(fahrenheit.toFixed(1));
+  return fahr;
 };
 
 // Do not edit below this line
