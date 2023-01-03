7e402b0475ddd338f9366e010355f170bb3adf95
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2023-01-03 11:24:15 +0100

    Update and add comment to findTheOldest function in exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,17 +1,56 @@
-const findTheOldest = function(array) {
-    return array.reduce((oldest, currentPerson) => {
-        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
-        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
-        return oldestAge < currentAge ? currentPerson : oldest;
-      })
-    };
-    
-    const getAge = function(birth, death) {
-      if (!death) {
-        death = new Date().getFullYear();
-      }
-      return death - birth;
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+const findTheOldest = function(people) {
+//   let oldestPerson ={};
+//   let oldestAge = 0;
+//   for(let i = 0; i < people.length; i++){
+//     let age = people[i].yearOfDeath - people[i].yearOfBirth;
+//     if(age >= oldestAge){
+//       oldestAge = age;
+//       oldestPerson = people[i];
+//     }
+//   }
+//   return oldestPerson;
+
+  // people.forEach(item => {
+  //   if(oldestAge < item.yearOfDeath - item.yearOfBirth){
+  //     oldestAge = item.yearOfDeath - item.yearOfBirth;
+  //     oldestPerson = item;
+  //   }
+  // })
+  // return oldestPerson;
+
+  return people.reduce((oldestPerson, oldestAlive) => {
+    const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+    const oldestAgeAlive = getAge(oldestAlive.yearOfBirth, oldestAlive.yearOfDeath);
+    return oldestAge < oldestAgeAlive ? oldestAlive : oldestPerson;
+  })
 };
+
+const getAge = function(birthYear, deathYear){
+  if (!deathYear){
+    deathYear = new Date().getFullYear();
+  }
+  return deathYear - birthYear;
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
