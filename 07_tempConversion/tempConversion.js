4651e888ee4b66ef0b4de6fc59fed53abbdd8f5c
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2023-01-03 08:50:32 +0100

    Add comment to tempCoversion function in exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
+// A function to convert temperature from Fahrenheit to Celsius.
 const convertToCelsius = function(tempInFarenheit) {
   const celsius = ((tempInFarenheit - 32) / 1.8);
   const cels = parseFloat(celsius.toFixed(1));
   return cels;
 };
 
+// A function to convert temperature from Celsius to Fahrenheit.
 const convertToFahrenheit = function(tempInCelsius) {
   const fahrenheit = ((tempInCelsius * 1.8) + 32);
   const fahr = parseFloat(fahrenheit.toFixed(1));
