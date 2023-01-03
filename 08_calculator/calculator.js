e15270843d0a97644d3cafe65871fad1e24260b1
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2023-01-03 09:10:17 +0100

    Add Comments to the functions in calculator directory

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,16 @@
+//A function to perform addition of two numbers
 const add = function(num1, num2) {
 	const addition = num1 + num2;
   return addition;
 };
 
+// A fuction to perform subtraction of two numbers
 const subtract = function(num1, num2) {
 	const subtraction = num1 - num2;
   return subtraction;
 };
 
+// A function to sum a set of nubers in an array
 const sum = function(numbers) {
   let summation = numbers.reduce(function(prev, curr){
     return curr + prev;
@@ -15,6 +18,7 @@ const sum = function(numbers) {
   return summation;
 };
 
+// A function to multiply a set of numbers in an array
 const multiply = function(numbers) {
   let multiplication = numbers.reduce(function(prev, curr){
     return curr * prev;
@@ -22,11 +26,13 @@ const multiply = function(numbers) {
   return multiplication;
 };
 
+// A function to perform the exponent(power) operation
 const power = function(number, power) {
 	const exp = number ** power;
   return exp;
 };
 
+// A function to perform the factorial operation on given numbers
 const factorial = function(number) {
 	if(number == 0 || number == 1){
     return 1;
